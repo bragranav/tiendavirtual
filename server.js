3c4b73739c9524c42c0b1b3608a7d4893e0e779b
Brayan Granados <braygranados114@gmail.com>
2024-11-19 17:34:45 -0500

    Guardar

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 
 // Configura CORS para permitir el acceso desde tu frontend en Netlify
 app.use(cors({
-    origin: 'https://sprightly-druid-1fb38a.netlify.app', // Reemplaza con la URL de tu frontend
+    origin: 'https://673c1d1eccf5d1b0266bb1a9--sprightly-druid-1fb38a.netlify.app', // Reemplaza con la URL de tu frontend
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true, // Permite el envío de cookies

769e97a16e4ab484f2edc391fe7176d427fa5c42
Brayan Granados <braygranados114@gmail.com>
2024-11-19 00:14:04 -0500

    Guardar

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true // Usa false si es un certificado autofirmado
+    },
     connectTimeout: 1000000
 });
 

67fe4adac60fdedf49d3c365202639e387235ed9
Brayan Granados <braygranados114@gmail.com>
2024-11-18 23:35:02 -0500

    Guardar

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Rutas
-app.get('/login.html', (req, res) => res.sendFile(path.join(__dirname, 'views/login.html')));
-app.get('/register.html', (req, res) => res.sendFile(path.join(__dirname, 'views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views/login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views/register.html')));
 app.get('/index', (req, res) => {
     // Asegúrate de que la ruta esté correcta
     res.sendFile(path.join(__dirname, '/index.html'));
